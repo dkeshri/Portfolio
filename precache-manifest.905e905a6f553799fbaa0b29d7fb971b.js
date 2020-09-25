a6f3fab3d9a8a5253f48f6136812223067285e12
Deepak Keshri <dkeshridev@gmail.com>
2020-09-25 19:30:07 +0530

    Updates

---
+++ precache-manifest.905e905a6f553799fbaa0b29d7fb971b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1be93d20c6e5a06cf0de25b35435863",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c17fac1674e98ea8f551",
+    "url": "/Portfolio/static/css/main.6e7a5d7b.chunk.css"
+  },
+  {
+    "revision": "bf437c7e33d205475aa0",
+    "url": "/Portfolio/static/js/2.01a4c4f6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.01a4c4f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c17fac1674e98ea8f551",
+    "url": "/Portfolio/static/js/main.b2397c5d.chunk.js"
+  },
+  {
+    "revision": "eca8a1a750d2c5b6ddc5",
+    "url": "/Portfolio/static/js/runtime-main.1d31859f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
