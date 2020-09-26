e0d005aeb13c7d46514bdf0defad97e46fc2d97b
Deepak Keshri <dkeshridev@gmail.com>
2020-09-26 12:41:40 +0530

    Updates

---
+++ precache-manifest.a9c4d4badf5abe3751c5c0e1ba82be08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ed075c56000e65dae782a235237d75",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "2a636302ede8e29a7dba",
+    "url": "/Portfolio/static/css/main.60e9ec09.chunk.css"
+  },
+  {
+    "revision": "9b089b39f0b9a1510a8f",
+    "url": "/Portfolio/static/js/2.0aa05ca5.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/Portfolio/static/js/2.0aa05ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a636302ede8e29a7dba",
+    "url": "/Portfolio/static/js/main.0efd848e.chunk.js"
+  },
+  {
+    "revision": "eca8a1a750d2c5b6ddc5",
+    "url": "/Portfolio/static/js/runtime-main.1d31859f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.2a43e1e5dc852e3450db418d115ddd29.js"
+  "/Portfolio/precache-manifest.a9c4d4badf5abe3751c5c0e1ba82be08.js"
 );
 
 self.addEventListener('message', (event) => {
