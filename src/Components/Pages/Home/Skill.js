b859189fec3d50e25393ce0a12ec838f80b88ca6
Deepak Keshri <dkeshridev@gmail.com>
2020-12-29 12:37:00 +0530

    added skills

--- src/Components/Pages/Home/Skill.js
+++ src/Components/Pages/Home/Skill.js
@@ -3,27 +3,20 @@ import '../../../assets/css/Pages/Home/Skill.css'
 import SkillCard from './SkillCard'
 function Skill() {
     return (
-        <div className="skill-container">
+        <div className="skills-container">
             <div className="skills-heading">
                 <div className="skill-heading-title">
                     <h1>Skills</h1>
                 </div>
             </div>
-            <div className="skills-container">
-                <div className="fornt-end">
-                    <div className="skill-title"><h3>Front-End Skills</h3></div>
-                    <SkillCard />
-                    <SkillCard />
-                    <SkillCard />
-                </div>
-                <div className="server-side">
-                    <div className="skill-title"><h3>Server-Side Skills</h3></div>
-                    <SkillCard />
-                </div>
-                <div className="back-end">
-                    <div className="skill-title"><h3>Back-End Skills</h3></div>
-                    <SkillCard />
-                </div>
+            <div className="skills-body-container">
+                <SkillCard iconCss="fa fa-html5" titles="Html 5"/>
+                <SkillCard iconCss="fa fa-css3" titles="Css"/>
+                <SkillCard iconCss="fa fa-html5" titles="Javascript"/>
+                <SkillCard iconCss="fa fa-html5" titles="React Js"/>
+                <SkillCard iconCss="fa fa-html5" titles="C#"/>
+                <SkillCard iconCss="fa fa-html5" titles="Asp .net"/>
+                <SkillCard iconCss="fa fa-html5" titles="SQL"/>
             </div>
 
         </div>
--- src/Components/Pages/Home/SkillCard.js
+++ src/Components/Pages/Home/SkillCard.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import '../../../assets/css/Pages/Home/SkillCard.css'
-function SkillCard() {
+function SkillCard(Props) {
     return (
         <div className="skill-card">
-            <i className="fa fa-html5"></i>
-            <div className="skill-progress-bar">
-                <div className="progress-indicator"></div>
-            </div>
-            <div className="percentage">
-                30%
-            </div>
+            <i className={Props.iconCss}></i>
+            <span class="skill-card-title">{Props.titles}</span>
         </div>
     )
 }
