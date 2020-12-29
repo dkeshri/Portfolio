8f49aa269f320bc439b196b941e2127562a6e716
Deepak Keshri <dkeshridev@gmail.com>
2020-12-29 12:50:50 +0530

    Updates

---
+++ precache-manifest.33b191f73b60b03c1cf1099d8bb6e5a5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64ff4d4de6c83895c11e7a7156ab3f8",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "4731fd8c0e3300631aaf",
+    "url": "/Portfolio/static/css/main.3707e0e1.chunk.css"
+  },
+  {
+    "revision": "b3be47150e42829d1a86",
+    "url": "/Portfolio/static/js/2.1bd98a22.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/Portfolio/static/js/2.1bd98a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4731fd8c0e3300631aaf",
+    "url": "/Portfolio/static/js/main.22ce6499.chunk.js"
+  },
+  {
+    "revision": "eca8a1a750d2c5b6ddc5",
+    "url": "/Portfolio/static/js/runtime-main.1d31859f.js"
+  },
+  {
+    "revision": "ab1b5c126919df11ff369b5feba5c29c",
+    "url": "/Portfolio/static/media/dkeshri.ab1b5c12.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1211f8383bd11f9f58bc1ad52db23fa4.js"
+  "/Portfolio/precache-manifest.33b191f73b60b03c1cf1099d8bb6e5a5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
